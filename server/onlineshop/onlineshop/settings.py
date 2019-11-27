a69dcf509e97c3f874236fc302949e5cd9cce469
micavanco <micavanco@gmail.com>
2019-11-27 22:30:27 +0100

    Added user serializer and REST framework

--- server/onlineshop/onlineshop/settings.py
+++ server/onlineshop/onlineshop/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shops.apps.ShopsConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- server/onlineshop/shops/views.py
+++ server/onlineshop/shops/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from rest_framework import viewsets
 
 
 def index(request):
     return HttpResponse("<h1>Hello from index</h1>")
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
